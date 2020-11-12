db77db92ffedbc6dd0f76858d42d5e0caa757c7d
jesse10930 <ronda.jesse@gmail.com>
2020-11-11 20:42:50 -0500

    preparing to deploy

--- back-end/server.js
+++ back-end/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 
 // init cross platform capability
@@ -16,6 +16,16 @@ app.use(express.json({ extended: false }))
 // Define Routes
 app.use('/api/foods', require('./routes/foods'));
 app.use('/api/remaining', require('./routes/remaining'));
+
+// Server static assets in production
+if(process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('../build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve('../build', 'index.html')));
+}
+
+const PORT = process.env.PORT || 5000;
 
 // start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
