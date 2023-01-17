779baa6eb56fe19203c7e756358a902c7a19fbc4
jesse10930 <ronda.jesse@gmail.com>
2023-01-16 19:42:29 -0500

    changing host

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 const path = require('path');
 const app = express();
 const cors = require('cors');
@@ -11,18 +11,20 @@ app.use(cors());
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }))
+app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/foods', require('./routes/foods'));
 app.use('/api/remaining', require('./routes/remaining'));
 
 // Server static assets in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname + 'client', 'build', 'index.html')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname + '/client', 'build', 'index.html'))
+  );
 }
 
 const PORT = process.env.PORT || 5000;
